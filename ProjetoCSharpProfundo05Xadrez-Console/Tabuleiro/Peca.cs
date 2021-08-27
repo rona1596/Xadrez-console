56450211640a0dbea6ccc1f5211732cc07523a8c
Ronaldo Silva <rona.jr96@gmail.com>
2021-08-27 09:58:18 -0300

    Starting to implement the board 27082021

---
+++ ProjetoCSharpProfundo05Xadrez-Console/Tabuleiro/Peca.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QteMovimentos { get; set; }
+        public Tabuleiro Tabuleiro { get; set; }
+
+        public Peca(Posicao posicao, Tabuleiro tabuleiro, Cor cor)
+        {
+            Posicao = posicao;
+            Tabuleiro = tabuleiro;
+            Cor = cor;
+            QteMovimentos = 0;
+        }
+    }
+}
