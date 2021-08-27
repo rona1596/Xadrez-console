60f00ae99d2ea06ddf0299932057f856cba5ac71
Ronaldo Silva <rona.jr96@gmail.com>
2021-08-27 08:47:51 -0300

    Project Started 27082021

---
+++ ProjetoCSharpProfundo05Xadrez-Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoCSharpProfundo05Xadrez_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
