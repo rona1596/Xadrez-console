16e6746219d61b65f5e8c08df04c9987a351a786
Ronaldo Silva <rona.jr96@gmail.com>
2021-09-04 11:37:50 -0300

    Creating the chess position 040920521

---
+++ ProjetoCSharpProfundo05Xadrez-Console/PecasXadrez/PosicaoXadrez.cs
@@ -0,0 +1,29 @@
+﻿using PastaTabuleiro;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoCSharpProfundo05Xadrez_Console.PecasXadrez
+{
+    class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        public Posicao ToPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Linha + Coluna;
+        }
+    }
+}
